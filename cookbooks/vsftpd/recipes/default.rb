1e069928b8c2b202c9971ba0b5ade7a0a6c967dd
Chris Fordham <chris@xhost.com.au>
2012-05-23 18:13:18 +1000

    Send log messages to debug only.

--- cookbooks/vsftpd/recipes/default.rb
+++ cookbooks/vsftpd/recipes/default.rb
@@ -16,13 +16,11 @@ end
 
 directory "/etc/vsftpd.userconf"
 
-if node[:vsftpd][:ssl_enabled]
-  
-  log 'Configuring for FTPS.'
-  
+if node[:vsftpd][:ssl_enabled]  
+  log('Configuring for FTPS.') { level :debug }
+
   if node[:vsftpd][:use_ssl_certs_from_cookbook]
-    
-    log "Installing SSL certificate and key for #{node['vsftpd']['ssl_certs_basename']}."
+    log("Installing SSL certificate and key for #{node['vsftpd']['ssl_certs_basename']}.") { level :debug }
     
     cookbook_file "#{node[:vsftpd][:ssl_cert_path]}/#{node[:vsftpd][:ssl_certs_basename]}.pem" do
       owner 'root'
